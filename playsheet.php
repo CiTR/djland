ee037b72a5c099b1d6acf4282964a62790c501ae
Evan Friday <evan_friday@hotmail.com>
2014-06-10 17:33:12 -0700

    fixing playsheet

--- playsheet.php
+++ playsheet.php
@@ -611,14 +611,14 @@ if (count($matches)>1){
 			printf("<FORM METHOD=POST ACTION=\"%s?action=submit\" name=\"playsheet\" id='playsheetForm' >", $_SERVER['SCRIPT_NAME']);
 		}
 		?>
-		<INPUT type=hidden id='psid' name=id value= <?php=$ps_id ?>>
+		<INPUT type=hidden id='psid' name=id value= <?php echo $ps_id; ?>>
 		<center><h1>DJ PLAYSHEET</h1></center>
 		<table border=0 align=center width=100%%><tr><td>Show Type:
 		<?php if( isset($loaded_type) && ($loaded_type != null) ){ ?>
-		<select id='type' name='type' value=".$loaded_type."><option selected><?php=$loaded_type ?></option>
-		<?php} 
+		<select id='type' name='type' value=".$loaded_type."><option selected><?php echo $loaded_type; ?></option>
+		<?php } 
 		else if(isset($showtype) && ($showtype != null)){ ?>
-		<select id='type' name='type' value=".$showtype."><option selected> <?php=$showtype ?> </option>		
+		<select id='type' name='type' value=".$showtype."><option selected> <?php echo $showtype; ?> </option>		
 		<?php }
 		else{ ?>
 		<select id='type' name='type' ><option selected>Live</option>
@@ -693,10 +693,10 @@ if (count($matches)>1){
 		?>
 		</SELECT>]
 		</td></tr><tr align=center width=400px>
-		<td>CRTC Category:<input type='text' id=pl_crtc name=pl_crtc value=<?php=$crtc_pl?>>
+		<td>CRTC Category:<input type='text' id=pl_crtc name=pl_crtc value=<?php echo $crtc_pl; ?>>
 		</td>
 		
-		<td align=right colspan=2>Language:<input type='text' id=pl_lang name=pl_lang value=<?=$lang_pl?>>		
+		<td align=right colspan=2>Language:<input type='text' id=pl_lang name=pl_lang value=<? echo $lang_pl ?>>		
 		<td/><tr/></table>
 		
 		<img src='images/loading.gif' id='ps-loading-image'>
@@ -706,9 +706,9 @@ if (count($matches)>1){
 		<span id='draft'><?php if($loaded_status==1):?>(draft)<?php endif;?></span>
 	
 		<br><table class='dragrows' id='playsheet-table'>
-		<input type='text' id='numberOfRows' name='numberOfRows' class='invisible' value='<?php=$playlist_entries?>'>
+		<input type='text' id='numberOfRows' name='numberOfRows' class='invisible' value='<?php echo $playlist_entries; ?>'>
 		<input type='text' id='numberOfAdRows' name='numberOfAdRows' class='invisible'>
-		<input type='text' id='unixTime' name='unixTime' class='invisible' value='<?php=$unix_start_time?>'>
+		<input type='text' id='unixTime' name='unixTime' class='invisible' value='<?php echo $unix_start_time; ?>'>
 		<input type='text' id='status' name='status' class='invisible' >
 		<input type='text' id='star' name='star' class='invisible' >	
 		<h2>Music</h2>	
