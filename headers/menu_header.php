3e6783c3f73864906fbff2eac202cf5ad0afdef0
Evan Friday <evan_friday@hotmail.com>
2014-06-11 01:28:51 -0700

    basis of membership established

--- headers/menu_header.php
+++ headers/menu_header.php
@@ -141,10 +141,10 @@ function membership_menu(){
 require('config.php');
 ?>
 <ul id ='tab-nav'>
-	<li class = 'nodrop active-tab'><a class='member_action' value='1'>Search Members</a></li>
-	<li class = 'nodrop inactive-tab'><a class='member_action' value='2'>Add Member</a></li>
-	<li class = 'nodrop inactive-tab'><a class='member_action' value='3'>Delete Member</a></li>
-	<li class = 'nodrop inactive-tab'><a class='member_action' value='4'>Send Emails</a></li>
+	<li class = 'nodrop active-tab member_action' value='init'>Search Members</li>
+	<li class = 'nodrop inactive-tab member_action' value='add'>Add Member</li>
+	<li class = 'nodrop inactive-tab member_action' value='remove'>Remove Member</li>
+	<li class = 'nodrop inactive-tab member_action' value='email'>Send Emails</li>
 </ul> 
 
 
--- js/membership.js
+++ js/membership.js
@@ -1,60 +1,105 @@
 
 
 $(document).ready ( function() {
-	manage_members(1);
+	manage_members('init');
+	add_handlers();
 	
-	$('.member_action').click( function () {
-		console.log("member_action");
-		var action = parseInt($(this).attr('value'));
-		manage_members(action);
-	});
-	$('#submit').click( function () {
-		var action = $(this).attr('name');
-		switch(action){
-			case add_member:
-				var name=document.getElementByName("name");
-				alert('Member added: ' + name);
-				manage_members(2);
-				break;
-			default:
-				manage_members(1);
-				break;
-		}
-	});
 });
 
-function manage_members(value){
+function add_handlers(){
+	$('.member_action').click( function () {
+		console.log("member_action");
+		var action = $(this).attr('value');
+		$('.member_action').attr('class','nodrop inactive-tab member_action');
+		$(this).attr('class','nodrop active-tab member_action');
+		manage_members(action);
+	});
+	
+	$('.member_submit').click( function () {
+		console.log("member_submit");
+		var action = $(this).attr('name');
+		switch(action){
+			case 'search':
+				var filter= document.getElementById("search_value").value;
+				manage_members(action,filter);
+				break;
+			case 'add':
+				var submit_name= document.getElementById("submit_name").value;
+				alert("Added: "+submit_name);
+				break;
+			case 'remove':
+				var submit_name= document.getElementById("submit_name").value;
+				alert("Removed: "+submit_name);
+				break;
+			case 'email':
+				var submit_name= document.getElementById("submit_name").value;
+				var message= document.getElementById("message").value;
+				alert("Emailed "+submit_name + ": "+ message);
+				break;
+			default:
+				manage_members("init");
+				break;
+		}
+	});
+}
+function manage_members(value_,filter_){
 		
 		//Values: 1 => Search Members, 2 => Add Member, 3 => Delete Member, 4=> Email Members 
-		var submenu_value = 1;
-		if(value){
-			submenu_value = value;
+		var value = null;
+		var filter = "";
+		if(value_){
+			value = value_;
 		}
-		document.getElementById("membership").innerHTML = " ";
 		
-		switch(submenu_value){
-			case 1: //Search
+		if(filter_){
+			filter = filter_;
+		}		
+		switch(value){
+			case 'init':
+				document.getElementById("membership").innerHTML = " ";
+				$('#membership').append("<input id='search_value' type='text' value='Search by Name'></input><button class='member_submit' name='search'>Search</button>");
+				$('#membership').append("<div id='member_result'></div>");
+				add_handlers();
+				break;
+			case 'search':
 				console.log('Search');
-				$('#membership').append("<input id='name' class='></input>");
+				document.getElementById("member_result").innerHTML = " ";
 				$.ajax({
-					type:"POST",
-					url: "form-handlers/membership-handler.php",
-					data: {"submenu_value" : submenu_value},
-					dataType: "json"
+				type:"POST",
+				url: "form-handlers/membership-handler.php",
+				data: {"value" : value, "filter" : filter},
+				dataType: "json"
 				}).success(function(data) {
 					for( $j = 0; $j < Object.keys(data).length; $j++ ){
-						$('#membership').append("<div>"+data[$j].firstname+" "+data[$j].lastname+"</div>")
+						$('#member_result').append("<div>"+data[$j].firstname+" "+data[$j].lastname+"</div>")
 					}
 				}).fail(function(){
 					$('#membership').html('connection error');
 				});
+				add_handlers();
 				break;
-			case 2: // Add Member
+				
+			case 'add': // Add Member
 				console.log('Add Member');
-				$('#membership').append('<input id = name></input><button id ="member_submit" name = "add_member">Add Member</button>');
+				document.getElementById("membership").innerHTML = " ";
+				$('#membership').append('<input id="submit_name" type="text" value="Enter a name" ></input><button class="member_submit" name="add">Add Member</button>');
+				add_handlers();
+				break;
+			case 'remove':
+				console.log('Remove Member');
+				document.getElementById("membership").innerHTML = " ";
+				$('#membership').append('<input  id="submit_name" type="text" value = "Enter a name"></input><button class="member_submit" name="remove">Remove Member</button>');
+				add_handlers();
+				break;
+			case 'email':
+				console.log('Email Members');
+				document.getElementById("membership").innerHTML = " ";
+				$('#membership').append('<input  id="submit_name" type="text" value ="Enter a name"></input><input id="message" type="text" value="Enter a message"></input><button class="member_submit" name = "email">Email Members</button>');
+				add_handlers();
 				break;
 			default:
-				manage_members(1);
+				manage_members('init');
+				add_handlers();
 				break;		
 		}	
 	}
