965babc48f459a6ce7fb73a6723bc35217988e53
Brad Winter <braddjwinter@gmail.com>
2014-08-14 15:32:55 -0700

    fix <? instead of <?php in charting handler flexible config.php requiring in case in a subfolder

--- headers/menu_header.php
+++ headers/menu_header.php
@@ -3,7 +3,7 @@
 
 function print_menu() {
 
-require('config.php');
+require('../config.php');
 printf("<link rel=stylesheet href=css/style.css type=text/css>");
 
 	printf("<table class=menu border=0 align=center><tr>");
