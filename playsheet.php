5f269a81d656938b47babc445abc1760cb1654c8
Evan Friday <evan_friday@hotmail.com>
2014-10-01 17:25:06 -0700

    hotfix

--- playsheet.php
+++ playsheet.php
@@ -313,13 +313,12 @@ else if($actionSet && $action == 'list' && !isset($_GET['delete'])) {
 	
 	printf("<INPUT type=hidden name=action value=edit>");
 	printf("<SELECT class='selectps' NAME=\"id\" SIZE=25>\n");
-	$get_playlists = "SELECT s.start_time AS start_time,p.id AS id, s.name AS name, p.star AS star, p.status AS status FROM playlists AS p INNER JOIN shows AS s ON s.id=p.show_id ORDER BY start_time DESC";
+	$get_playlists = "SELECT p.start_time AS start_time,p.id AS id, s.name AS name, p.star AS star, p.status AS status FROM playlists AS p INNER JOIN shows AS s ON s.id=p.show_id   ORDER BY start_time DESC";
 	if($result = $db->query($get_playlists)){
 
 
 		while($row = mysqli_fetch_array($result)){
 			$time = date( 'Y: M j, g:ia' ,strtotime($row['start_time']));
-			if($row['status']==1)
 			echo "<option value='".$row[id]."'>".$time." - ".$row[name].$row['status'] = 1 ? " - (draft)":"".$row["star"] = 1 ? "&#9733":""."</option>";
 		}
 	}
