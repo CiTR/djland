f41584ff645fa672f434a2e3d55000f88c683934
brad winter <braddjwinter@gmail.com>
2015-05-29 12:14:02 -0700

    .

--- podcasts.php
+++ podcasts.php
@@ -291,7 +291,7 @@ djland.controller('episodeList', function($scope, apiService, $location, $filter
 
             $scope.plodcasts = [].concat(response.data);
 
-            
+
 
             $scope.status = 'select a plodcast';
 
