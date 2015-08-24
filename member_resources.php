672eeb82cacc39f261045a26d23aa1290c6c7619
scottpidzarko <scott.pidzarko@gmail.com>
2015-08-23 20:40:15 -0700

    Changed style.css to do ordered lists and unordered lists correctly. Finished member resources page (need to do studio booking YCBM later). Fixed look of studio booking page. Fixed minor gramamr on Ad Scheduler page.

--- member_resources.php
+++ member_resources.php
@@ -5,6 +5,7 @@ require_once("headers/security_header.php");
 require_once("headers/functions.php");
 require_once("headers/menu_header.php");
 
+
 error_reporting(E_ALL);
 ?>
 
@@ -13,77 +14,38 @@ error_reporting(E_ALL);
 		<meta name=ROBOTS content="NOINDEX, NOFOLLOW">
 		<base href='shows.php'>
 		<link rel="stylesheet" href="css/style.css" type="text/css">
-
-		<style type="text/css">
-
-			#mainpodcast {
-				font-size:1.10em;
-				line-height:1.3em;
-			}
-
-			#mainpodcast input, #mainpodcast textarea{
-				width:100%;
-				margin: 5px;
-				padding:5px;
-			}
-
-			#mainpodcast input{
-				font-size: 1em;
-			}
-			#mainpodcast textarea{
-				font-size:0.77em;
-			}
-		</style>
+		<title>DJLAND | Resources</title>
 	</head>
 
-	<body>
+	<body class='wallpaper'>
 
-		<?php print_menu(); ?>
+	<?php print_menu(); ?>
 
-		<div ng-app="djLand" id="mainpodcast" ng-cloak>
-
-			<div ng-controller="episodeSingle" >
-				{{status}}<hr/>
-				{{episode}}<hr/>
-
-				<div ng-controller="episodeCtrl" ng-repeat="episode in episodes" class="form_wrap show_form">
-
-					<h3>editing podcast episode</h3>
-
-					<h3>{{episodeData.title}}</h3>
-
-					Episode Title:<br/>
-					<input ng-model="episode.title">
-					</input><br/>
-
-					Subtitle:<br/>
-					<input  ng-model="episode.subtitle" >
-					</textarea><br/>
-
-					Episode Summary:<br/>
-					<textarea ng-model="episode.summary" rows="25">
-					</textarea><br/>
-
-					Date:<br/>
-					<input ng-model="episode.date">
-					</input><br/>
-
-					URL:<br/>
-					<input ng-model="episode.url">
-					</input><br/>
-
-					message:{{message}}<br/>
-
-
-					<button ng-click="save(episode);" >save info (tba)</button>
-					<textarea cols="100" rows="20">{{episode}}</textarea>
-				</div>
-			</div>
+		<div id="Member_resources" class="center">
+			<p><strong>Member resources:</strong></p>
+			<ul type="disc">
+				<li>Our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2012/03/CiTR%20Policy%20-%20Final%20Draft.pdf');"  href="http://www.citr.ca/wp-content/uploads/2012/03/CiTR%20Policy%20-%20Final%20Draft.pdf">CiTR Programming Policies</a>, <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2012/03/CiTR%20Codes%20of%20Conduct%20-%20(web)%20Final%20Draft.pdf');"  href="http://www.citr.ca/wp-content/uploads/2012/03/CiTR%20Codes%20of%20Conduct%20-%20(web)%20Final%20Draft.pdf">CiTR On-Air Codes of Conduct</a> and <a onclick="javascript:pageTracker._trackPageview('/outgoing/ncra.ca/compliance-resources');"  href="http://ncra.ca/compliance-resources">NCRA Regulatory Guide</a> are the basics for broadcasters. Get to know them.</li>
+				<li>The <a href="http://www.citr.ca/index.php/crtc-pages-on-everything/">CRTC pages on everything</a> broadcast! Music categories, election campaigns, best practices, Canadian Content, Turntablism, etc.</li>
+				<li>Our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/DJ-Land-2.0.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/DJ-Land-2.0.pdf">Programmer Training Manual</a> outlines your on-air role and responsibilities</li>
+				<li>Our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2010/01/Setting-Up-Your-Show.pdf');"  href="http://www.citr.ca/wp-content/uploads/2010/01/Setting-Up-Your-Show.pdf">Setting Up Your Show</a> guide gets your podcasts going</li>
+				<li>Our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/Marantz-User-Guide1.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/Marantz-User-Guide1.pdf">Marantz User Guide</a> gives you cheat notes on how to use our Marantz digital recorders, and the proper <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/Marantz-Manual.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/Marantz-Manual.pdf">Marantz Manual</a> will explain everything in great detail</li>
+				<li>Our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/Zoom-User-Guide.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/Zoom-User-Guide.pdf">Zoom User Guide</a> gives you cheat notes on how to use our Zoom digital recorders, and the full <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/H2_user_manual.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/H2_user_manual.pdf">ZoomH2 Manual</a> will explain everything in great detail</li>
+				<li>Our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/LIVE-BROADCAST-PROPOSAL-FORM.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/LIVE-BROADCAST-PROPOSAL-FORM.pdf">Live Broadcasting Proposal Form</a> for programmers who want to broadcast live on location</li>
+				<li>Our 	<a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/Live-Broadcasting-Manual.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/Live-Broadcasting-Manual.pdf">Live Broadcasting Manual</a> will tell you everything to know about broadcasting live</li>
+				<li>Here is <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/REMOTE-BROADCASTING-WORKSHOP-with-TYLER-NOBLE.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/REMOTE-BROADCASTING-WORKSHOP-with-TYLER-NOBLE.pdf"> Tyler Noble&#8217;s Remote Broadcasting workshop</a></li>
+				<li>Our Programmers Forum <a onclick="javascript:pageTracker._trackPageview('/outgoing/citrradio.proboards.com/index.cgi');"  href="http://citrradio.proboards.com/index.cgi">CiTR Programmers Forum</a></li>
+				<li>Interested in spoken word? Read our <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/SpokenWordFAQ.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/SpokenWordFAQ.pdf">Spoken Word FAQ</a></li>
+				<li>Watch this video to learn <a onclick="javascript:pageTracker._trackPageview('/outgoing/www.youtube.com/watch?v=xvJciUR_ZGU');"  href="http://www.youtube.com/watch?v=xvJciUR_ZGU">How to Submit Your Podcasts to itunes</a>.</li>
+				<li>Check out this <a onclick="javascript:pageTracker._trackPageview('/downloads/wp-content/uploads/2009/06/Trans-Style-Guide.pdf');"  href="http://www.citr.ca/wp-content/uploads/2009/06/Trans-Style-Guide.pdf">Trans Style Guide</a> to help you write clearly, consistently and respectfully about trans issues.</li>
+			</ul>
+			<p><strong>Training</strong></p>
+			<p>Sign up for our training tutorials (FORMS COMING SOON):</p>
+			<ol>
+				<li>Technical</li>
+				<li>Programming</li>
+				<li>Production</li>
+			</ol>
+			<p>You must sign up in order of sessions offered above and at least 24 hours in advance. We recommend coming in for a tour first. If you are not able to physically sign up at the station, please send us an email at <a onclick="javascript:pageTracker._trackPageview('/mailto/programming@citr.ca');"  href="mailto:programming@citr.ca">programming@citr.ca</a>.</p>
 		</div>
-			<script src="js/angular.js"></script>
-			<script type="text/javascript">
-				var djland = angular.module('djLand', []);
-			</script>
-			<script src="js/angular-djland.js"></script>
 	</body>
 </html>
--- studio_booking.php
+++ studio_booking.php
@@ -8,7 +8,7 @@ require_once("headers/menu_header.php");
 error_reporting(E_ALL);
 ?>
 
-	<html>
+<html>
 	<head>
 		<meta name=ROBOTS content="NOINDEX, NOFOLLOW">
 		<base href='shows.php'>
@@ -20,24 +20,30 @@ error_reporting(E_ALL);
 
 	<?php print_menu(); ?>
 
+		<br/>
 		<h1>Studio Booking</h1>
 
-		<div>
-			<p><i>*If you book studio time, you have a fifteen minute grace period to show up on time. If not, the studio will be claimed on a first-come, first-serve basis for interested members.</i><br/>
-
-			<a name="studiocbooking"></a><br />
-			<iframe src="http://citrstudioc.youcanbook.me" class='center' style="border:0px;background:transparent;" frameborder="0" allowtransparency="true"></iframe></p>
-			<p><a onclick="javascript:pageTracker._trackPageview('/outgoing/www.google.com/calendar/embed?src=citr.ca_enon4vvn43udfr5p8d6p4bb4pg%40group.calendar.google.com&#038;ctz=America/Vancouver');" style="border: 0px; background: transparent;" title="See the Studio B bookings here"  href="http://www.google.com/calendar/embed?src=citr.ca_enon4vvn43udfr5p8d6p4bb4pg%40group.calendar.google.com&#038;ctz=America/Vancouver" target="_blank">See the Studio C bookings here</a><br />
-
-			<a name="studiobbooking"></a><br />
-			<iframe src="http://citrstudiob.youcanbook.me" class='center' style="border:0px;background:transparent;" frameborder="0" allowtransparency="true"></iframe></p>
-			<p><a onclick="javascript:pageTracker._trackPageview('/outgoing/www.google.com/calendar/embed?src=citr.ca_alhprjo077j16o3tp2r468mucg%40group.calendar.google.com&amp;ctz=America/Vancouver');" style="border: 0px; background: transparent;" title="See the Studio B bookings here"  href="http://www.google.com/calendar/embed?src=citr.ca_alhprjo077j16o3tp2r468mucg%40group.calendar.google.com&amp;ctz=America/Vancouver" target="_blank">See the Studio B bookings here</a></p>
-
-			<p>If you would like to book a time that is taken contact the person in the link above or CiTR staff to reschedule.</p>
-
-			<p><em>If any problems occur with your booking, please email <a onclick="javascript:pageTracker._trackPageview('/mailto/technicalassistant@citr.ca mailto:technicalservices@citr.ca');"  href="mailto:technicalassistant@citr.ca mailto:technicalservices@citr.ca">tech services</a>.</em></p>
+		<div class="center">
+			<div class="center" style="max-width:830px">
+				<p><i>*If you book studio time, you have a fifteen minute grace period to show up on time. If not, the studio will be claimed on a first-come, first-serve basis for interested members.</i><br/>
+			</div>
+			<div class="youCanBookMe_Container">
+				<iframe src="http://citrstudiob.youcanbook.me" style="border:0px;background:transparent;" frameborder="0" allowtransparency="true"></iframe></p>
+			</div>
+			<div class="center" style="max-width:830px">
+				<p><a onclick="javascript:pageTracker._trackPageview('/outgoing/www.google.com/calendar/embed?src=citr.ca_alhprjo077j16o3tp2r468mucg%40group.calendar.google.com&amp;ctz=America/Vancouver');" style="border: 0px; background: transparent;" title="See the Studio B bookings here"  href="http://www.google.com/calendar/embed?src=citr.ca_alhprjo077j16o3tp2r468mucg%40group.calendar.google.com&amp;ctz=America/Vancouver" target="_blank">See the Studio B bookings here</a></p>			
+			</div>
+			<div class="youCanBookMe_Container">
+				<iframe src="http://citrstudioc.youcanbook.me" style="border:0px;background:transparent;" frameborder="0" allowtransparency="true"></iframe></p>
+			</div>
+			<div class="center" style="max-width:830px">
+				<p><a onclick="javascript:pageTracker._trackPageview('/outgoing/www.google.com/calendar/embed?src=citr.ca_enon4vvn43udfr5p8d6p4bb4pg%40group.calendar.google.com&#038;ctz=America/Vancouver');" style="border: 0px; background: transparent;" title="See the Studio B bookings here"  href="http://www.google.com/calendar/embed?src=citr.ca_enon4vvn43udfr5p8d6p4bb4pg%40group.calendar.google.com&#038;ctz=America/Vancouver" target="_blank">See the Studio C bookings here</a><br />
+			</div>
+			<div class="center" style="max-width:830px">
+				</br>		
+				<p>If you would like to book a time that is taken contact the person in the link above or CiTR staff to reschedule.</p>
+				<p><em>If any problems occur with your booking, please email <a onclick="javascript:pageTracker._trackPageview('/mailto/technicalassistant@citr.ca mailto:technicalservices@citr.ca');"  href="mailto:technicalassistant@citr.ca mailto:technicalservices@citr.ca">tech services</a>.</em></p>
+			</div>
 		</div>
-
-
 	</body>
 </html>
