018710fa8a5359032ebd85ec2d5d8c8ed88436f3
EvanFriday <evan_friday@hotmail.com>
2015-09-02 16:25:57 -0700

    copying assembly api v

--- api/shows/index.php
+++ api/shows/index.php
@@ -10,7 +10,6 @@ require_once('../api_common.php');
 
 $offset = isset($_GET['OFFSET']) && is_numeric($_GET['OFFSET']) ? $_GET['OFFSET'] * 1 : 0;
 $limit  = isset($_GET['LIMIT'])  && is_numeric($_GET['LIMIT'])  ? $_GET['LIMIT']  * 1 : 100;
-
 $query =   "SELECT 	shows.id as id,
 					GREATEST(shows.edit_date,COALESCE(podcast_channels.edit_date,'0000-00-00 00:00:00')) as edit_date
 			FROM shows
@@ -18,16 +17,10 @@ $query =   "SELECT 	shows.id as id,
 			ORDER BY GREATEST(shows.edit_date,COALESCE(podcast_channels.edit_date,'0000-00-00 00:00:00')) DESC
 			LIMIT $limit
 			OFFSET $offset";
-
 $data = array();
-
 if ($result = mysqli_query($db, $query) ) {
-
   while ($row = mysqli_fetch_assoc($result)) {
-
     $data[] = $row;
-
   }
 }
-
 finish();
