5af3780aaca629a2dca1f153c1aa1e996d65cda5
EvanFriday <evan_friday@hotmail.com>
2015-10-07 19:12:46 -0700

    using proper primary key

--- api2/app/Categorylist.php
+++ api2/app/Categorylist.php
@@ -9,6 +9,7 @@ class Categorylist extends Model
     //Please Note: This is a model for the SAM database, not the local DJLand database
     protected $connection = 'samdb';
     protected $table = 'Categorylist';
+    protected $primaryKey = 'ID';
     public function songlist(){
     	return $this->belongsToMany('App\Songlist','ID','songID');
     }
