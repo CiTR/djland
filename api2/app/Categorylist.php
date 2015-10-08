568bb3bcc2d5cb43c986613a96aaee977c6f03c1
EvanFriday <evan_friday@hotmail.com>
2015-10-07 19:18:17 -0700

    changing primary key to catID

--- api2/app/Categorylist.php
+++ api2/app/Categorylist.php
@@ -9,7 +9,7 @@ class Categorylist extends Model
     //Please Note: This is a model for the SAM database, not the local DJLand database
     protected $connection = 'samdb';
     protected $table = 'Categorylist';
-    protected $primaryKey = 'ID';
+    protected $primaryKey = 'categoryID';
     public function songlist(){
     	return $this->belongsTo('App\Songlist','ID','songID');
     }
