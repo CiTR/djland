da4486f189396c852994383d6aaf72480e9adea2
Evan Friday <evanjohnfriday@gmail.com>
2015-10-13 15:27:47 -0700

    adding brackets to ensure filter by date

--- form-handlers/samAds_handler.php
+++ form-handlers/samAds_handler.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 $today = date('Y-m-d');
 $from = $today;
 $to = $today;
+$adname = "";
 $filter = false;
 if(isset($_POST['from'])){
 	$from = str_replace('/','-',$_POST['from'])." 00:00:00";
@@ -15,15 +16,19 @@ if(isset($_POST['from'])){
 	$from = $from." 00:00:00";
 	$to = $to." 23:59:59";
 }
-if(isset($_POST['adname'])){
+if(isset($_POST['adname']) && $_POST['adname'] != ""){
 	$adname = $_POST['adname'];
 	$filter = true;
 }
+echo "From ".$from;
+echo "To ".$to;
+echo "Term ".$adname;
 if($filter == true){
-	$query = "SELECT filename, date_played FROM historylist WHERE date_played >= '{$from}' AND date_played <= '{$to}' AND songtype = 'A' AND filename LIKE '%".$adname."%' OR title LIKE '%".$adname."%' OR artist LIKE '%".$adname."%' ORDER BY date_played DESC";
+	$query = "SELECT filename, date_played FROM historylist WHERE date_played > '{$from}' AND date_played <= '{$to}' AND songtype = 'A' AND (filename LIKE '%".$adname."%' OR title LIKE '%".$adname."%' OR artist LIKE '%".$adname."%') ORDER BY date_played DESC";
 }else{
-	$query = "SELECT filename, date_played FROM historylist WHERE date_played >= '{$from}' AND date_played <= '{$to}' AND songtype = 'A' ORDER BY date_played DESC";
+	$query = "SELECT filename, date_played FROM historylist WHERE date_played > '{$from}' AND date_played <= '{$to}' AND songtype = 'A' ORDER BY date_played DESC";
 }
+echo "Query: ".$query;
 
 if($result = $mysqli_sam->query($query)){
 	$adPlays=array();
