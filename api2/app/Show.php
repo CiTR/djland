695051eba2eb6f6a9d91bf785f6f23a6803b82ce
Evan Friday <evanjohnfriday@gmail.com>
2016-01-21 23:10:33 -0800

    change limit to 300 items

--- api2/app/Show.php
+++ api2/app/Show.php
@@ -190,7 +190,7 @@ class Show extends Model
         $key = array_keys($episodes->toArray());
         $num = count($key);
         if($testing_environment) $num = 6;
-        else $num = 100;
+        else $num = 300;
         $i = 0;
         $count = 0;
         while( $count < $num ) {
