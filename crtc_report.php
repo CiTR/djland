8a5cbba0fe69a8ad228a5bb41f158502f434a18f
Evan Friday <evanjohnfriday@gmail.com>
2016-01-22 17:58:44 -0800

    grid is working

--- crtc_report.php
+++ crtc_report.php
@@ -12,7 +12,7 @@
 			<button ng-click='report.toggle_print()' id='print_friendly'>Print Friendly View</button>
 			<ul id='filter_bar' class='text-center inline-list'>
 				<li>
-					Filter By Show: 
+					Filter By Show:
 					<select ng-model='report.show_filter'>
 						<div ng-if="{{list.is_admin}}">
 							<option selected value='all'>All Shows</option>
@@ -22,7 +22,7 @@
 				</li>
 				<li class='side-padded'>
 					<div class="dropdown">
-						<div ng-controller="datepicker as date"> 
+						<div ng-controller="datepicker as date">
 			        		<button ng-click="date.open($event)" >Change Start Day</button>
 			        		<input class="date_picker" type="text" datepicker-popup="yyyy/MM/dd"
 				                 ng-model="report.from"  is-open="date.opened"
@@ -60,11 +60,72 @@
 					<button type='button' id='generate' ng-click='report.report()'>Generate Report</button>
 				</li>
 			</ul>
-			<div >
+			<div class='text-center' ng-hide='report.playsheets.length > 0'>
+				No Results
+			</div>
+
+			<div  ng-show='report.playsheets.length > 0'>
+				<h3>Summary</h3>
+				<h4>{{report.from | date:'yyyy/MM/dd'}} - {{report.to | date:'yyyy/MM/dd'}}</h4>
+
+
+					<table class='table-condensed crtc_report'>
+						<tr>
+							<th>
+								<div ng-if='report.show_names.length > 1'>Show Name</div>
+								<div ng-if='report.show_names.length == 1'>Date</div>
+							</th>
+							<th>Canadian(30)</th>
+							<th>Canadian(20)</th>
+							<th>Femcon</th>
+							<th>Hits</th>
+						</tr>
+
+						<tr ng-repeat="item in report.percentages track by $index | orderBy:'item.name'">
+							<td>
+								<div ng-if='report.show_names.length > 1'>{{item.name}}</div>
+								<div ng-if='report.show_names.length == 1'>{{item.date}}</div>
+
+							</td>
+							<td ng-class='(item.cancon_3_total/item.playitems_3)*100 >= item.required_cancon || item.playitems_3 == 0 ? "":"red"'>
+								{{item.cancon_3_total}} / {{item.playitems_3}}
+								({{item.cancon_3_total/item.playitems_3 > 0 ? item.cancon_3_total/item.playitems_3 : 0 | percentage:0}}/{{item.required_cancon}}%)
+							</td>
+							<td ng-class='(item.cancon_2_total/item.playitems_2)*100 >= item.required_cancon || item.playitems_2 == 0 ? "":"red"'>
+								{{item.cancon_2_total}} / {{item.playitems_2}}
+								({{item.cancon_2_total/item.playitems_2 > 0 ? item.cancon_2_total/item.playitems_2 : 0 | percentage:0}}/{{item.required_cancon}}%)
+							</td>
+							<td ng-class='(item.femcon_total/item.total)*100 >= item.required_femcon || item.total == 0 ? "":"red"'>
+								{{item.femcon_total}} / {{item.total}}
+								({{item.femcon_total/item.total > 0 ? item.femcon_total/item.total : 0 | percentage:0}}/{{item.required_femcon}}%)
+							</td>
+						</tr>
+
+						<tr>
+							<td>Total</td>
+							<td ng-class='(report.percentage_totals.cancon_3_total/report.percentage_totals.playitems_3*100) >= 35 || report.percentage_totals.playitems_3 == 0 ?"":"red"'>
+								{{report.percentage_totals.cancon_3_total}} / {{report.percentage_totals.playitems_3}}
+								({{report.percentage_totals.cancon_3_total/report.percentage_totals.playitems_3 > 0 ? report.percentage_totals.cancon_3_total/report.percentage_totals.playitems_3 : 0 | percentage:0}}/35%)
+							</td>
+							<td ng-class='(report.percentage_totals.cancon_2_total/report.percentage_totals.playitems_2*100) >= 35  || report.percentage_totals.playitems_2 == 0 ?"":"red"'>
+								{{report.percentage_totals.cancon_2_total}} / {{report.percentage_totals.playitems_2}}
+								({{report.percentage_totals.cancon_2_total/report.percentage_totals.playitems_2 > 0 ? report.percentage_totals.cancon_2_total/report.percentage_totals.playitems_2 : 0 | percentage:0}}/35%)
+							</td>
+							<td ng-class='(report.percentage_totals.femcon_total/report.percentage_totals.playitems*100) >= 35 || report.percentage_totals.playitems == 0 ?"":"red"'>
+								{{report.percentage_totals.femcon_total}} / {{report.percentage_totals.playitems}}
+								({{report.percentage_totals.femcon_total/report.percentage_totals.playitems > 0 ? report.percentage_totals.femcon_total/report.percentage_totals.playitems : 0 | percentage:0}}/35%)
+							</td>
+						</tr>
+					</table>
+
+
+			</div>
+			<div>
 				<div ng-repeat='playsheet in report.playsheets track by $index' class='report_item' reportitem ></div>
 			</div>
 		</div>
 
+		<tr>
 		<script type='text/javascript' src='js/angular.js'></script>
 		<script type='text/javascript' src='js/jquery-1.11.3.min.js'></script>
 		<script type='text/javascript' src='js/bootstrap/ui-bootstrap-tpls-0.12.0-withseconds.js'></script>
--- js/report/report.js
+++ js/report/report.js
@@ -1,7 +1,7 @@
 (function (){
 	var app = angular.module('djland.report',['djland.api','ui.bootstrap']);
 
-	app.controller('reportController',function(call,$filter){
+	app.controller('reportController',function(call,$filter,$scope){
 		this.show_filter = 'all';
 		var date = new Date();
 		this.to = $filter('date')(date,'yyyy/MM/dd');
@@ -16,31 +16,28 @@
 				this_.shows = response.data.shows;
 			});
 			call.getMemberPermissions(this.member_id).then(function(response){
-                if(response.data.administrator == '1' || response.data.staff == '1' ){
-                    this_.is_admin = true;
-                }else{
-                    this_.is_admin = false;
-                }
-            },function(error){
-                  
-            });
-			this.report();
+				if(response.data.administrator == '1' || response.data.staff == '1' ){
+					this_.is_admin = true;
+				}else{
+					this_.is_admin = false;
+				}
+			},function(error){
+
+			});
+			call.getReport(this.show_filter,$filter('date')(this.from, 'yyyy/MM/dd'),$filter('date')(this.to,'yyyy/MM/dd')).then(function(response){
+				this_.playsheets = angular.copy(response.data);
+				this_.loadGrid(response);
+			});
 		}
 		this.report = function(){
 			this_ = this;
-
 			call.getReport(this.show_filter,$filter('date')(this.from, 'yyyy/MM/dd'),$filter('date')(this.to,'yyyy/MM/dd')).then(function(response){
-				this_.playsheets = angular.copy(response.data);
-				var length = this_.playsheets.length;
-				for(var i = 0; i < length; i++){
-					if(this_.show_names.indexOf(this_.playsheets[i].show.name) < 0){
-						this_.show_names.push(this_.playsheets[i].show.name);
-					}
-				}
-
-				
-
+				this_.playsheets = response.data.length > 0 ? angular.copy(response.data) : Array();
+				setTimeout(function(){
+					this_.loadGrid();
+				},500);
 			});
+
 		}
 		this.toggle_print = function(element){
 			var button = $('#print_friendly');
@@ -48,15 +45,101 @@
 				button.text("Normal View");
 				$('#nav, #filter_bar').hide();
 				$('body').removeClass('wallpaper');
+				$('.red').addClass('grey');
+				$('.red').toggleClass('red');
 				$('.crtc_report').addClass('print_wrapper');
 			}else{
-			  	button.text("Print Friendly View");
-			  	$('#nav, #filter_bar').show();
+				button.text("Print Friendly View");
+				$('#nav, #filter_bar').show();
 				$('body').addClass('wallpaper');
 				$('.crtc_report').removeClass('print_wrapper');
+				$('.grey').addClass('red');
+				$('.grey').toggleClass('grey');
 			}
-				
-			
+
+
+		}
+		this.loadGrid = function(){
+			var this_ = this;
+			this.percentages = {};
+			this.percentage_totals = {};
+			$scope.$apply();
+			this.show_names = Array();
+			var length = this.playsheets.length;
+			//console if no playitems, just return
+			if(length <= 0)	return -1;
+
+			//Generate list of show names
+			for(var i = 0; i < length; i++){
+				if(this.show_names.indexOf(this.playsheets[i].show.name) < 0) this.show_names.push(this.playsheets[i].show.name);
+			}
+
+			//Set defaults for the overall totals
+			this.percentage_totals = {'playitems':0,'playitems_2':0,'playitems_3':0,'femcon_total':0,'cancon_2_total':0,'cancon_3_total':0};
+
+			//Check to see if there is only one show, or multiple
+			if(this.show_names.length > 1){
+				//Get playsheets under their respective show names as we have multiple shows
+
+				for(var index in this.show_names){
+					//Get list of applicable playsheets
+					var show_playsheets = this.playsheets.filter(function(obj){ return obj.show.name == this_.show_names[index]; });
+					//Get the show info for the show name
+					var show = this.shows.filter(function(obj){return obj.name==this_.show_names[index]; })[0].show;
+					//Set defaults for the show percentages
+					this.percentages[this.show_names[index]] = {'name':this.show_names[index],'playitems_2':0,'playitems_3':0,'total':0,'cancon_2_total':0,'cancon_3_total':0,'femcon_total':0};
+					this.percentages[this.show_names[index]]['required_cancon'] = show.cc_req;
+					this.percentages[this.show_names[index]]['required_femcon'] = show.fem_req;
+
+					for(var item in show_playsheets){
+						var playitems = show_playsheets[item].playitems;
+						//Add totals to the show total so far
+						this.percentages[this.show_names[index]]['total'] += playitems.length;
+						this.percentages[this.show_names[index]]['playitems_2'] += playitems.filter(function(obj){return obj.crtc_category == 20}).length;
+						this.percentages[this.show_names[index]]['playitems_3'] += playitems.filter(function(obj){return obj.crtc_category == 30}).length;
+						//Add count to the show count so far
+						this.percentages[this.show_names[index]]['cancon_2_total'] += playitems.filter(function(obj){return obj.is_canadian==1 && obj.crtc_category == 20}).length;
+						this.percentages[this.show_names[index]]['cancon_3_total'] += playitems.filter(function(obj){return obj.is_canadian==1 && obj.crtc_category == 30}).length;
+						this.percentages[this.show_names[index]]['femcon_total'] += playitems.filter(function(obj){return obj.is_fem==1}).length;
+					}
+					//Add show total to overall totals
+					this.percentage_totals.playitems += this.percentages[this.show_names[index]]['total'];
+					this.percentage_totals.playitems_2 += this.percentages[this.show_names[index]]['playitems_2'];
+					this.percentage_totals.playitems_3 += this.percentages[this.show_names[index]]['playitems_3'];
+					this.percentage_totals.cancon_2_total += this.percentages[this.show_names[index]]['cancon_2_total'];
+					this.percentage_totals.cancon_3_total += this.percentages[this.show_names[index]]['cancon_3_total'];
+					this.percentage_totals.femcon_total += this.percentages[this.show_names[index]]['femcon_total'];
+				}
+			}else{
+				//Playsheets are all from one show.
+
+				//Get the show information for this name
+				var show = this.shows.filter(function(obj){return obj.name==this_.show_names[0]; })[0].show;
+				for(var item in this.playsheets){
+					var playsheet = this.playsheets[item];
+					var playitems = playsheet.playitems;
+					//Set defaults for this playsheet
+					this.percentages[playsheet.id] = {'date':playsheet.start_time,'required_cancon':show.cc_req,'required_femcon':show.fem_req};
+					//get totals for the playsheet
+					this.percentages[playsheet.id]['total'] = playitems.length;
+					this.percentages[playsheet.id]['playitems_2'] = playitems.filter(function(obj){return obj.crtc_category == 20}).length;
+					this.percentages[playsheet.id]['playitems_3'] = playitems.filter(function(obj){return obj.crtc_category == 30}).length;
+					//Count values from playsheet
+					this.percentages[playsheet.id]['cancon_2_total'] = playitems.filter(function(obj){return obj.is_canadian==1 && obj.crtc_category == 20}).length;
+					this.percentages[playsheet.id]['cancon_3_total'] = playitems.filter(function(obj){return obj.is_canadian==1 && obj.crtc_category == 30}).length;
+					this.percentages[playsheet.id]['femcon_total'] = playitems.filter(function(obj){return obj.is_fem==1}).length;
+					//increment overall counts
+					this.percentage_totals.playitems += this.percentages[playsheet.id]['total'];
+					this.percentage_totals.playitems_2 += this.percentages[playsheet.id]['playitems_2'];
+					this.percentage_totals.playitems_3 += this.percentages[playsheet.id]['playitems_3'];
+					this.percentage_totals.cancon_2_total += this.percentages[playsheet.id]['cancon_2_total'];
+					this.percentage_totals.cancon_3_total += this.percentages[playsheet.id]['cancon_3_total'];
+					this.percentage_totals.femcon_total += this.percentages[playsheet.id]['femcon_total'];
+					console.log(this.percentage_totals);
+					$scope.$apply();
+
+				}
+			}
 		}
 		this.init();
 	});
@@ -82,18 +165,18 @@
 		}
 	});
 	app.filter('pad', function () {
-	  return function (n, len) {
-	    var num = parseInt(n, 10);
-	    len = parseInt(len, 10);
-	    if (isNaN(num) || isNaN(len)) {
-	      return n;
-	    }
-	    num = ''+num;
-	    while (num.length < len) {
-	      num = '0'+num;
-	    }
-	    return num;
-	  };
+		return function (n, len) {
+			var num = parseInt(n, 10);
+			len = parseInt(len, 10);
+			if (isNaN(num) || isNaN(len)) {
+				return n;
+			}
+			num = ''+num;
+			while (num.length < len) {
+				num = '0'+num;
+			}
+			return num;
+		};
 	});
 	app.filter('percentage', ['$filter', function ($filter) {
 		return function (input, decimals) {
