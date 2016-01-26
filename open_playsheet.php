bc3bee0464b0e4c5e7efa6b87be117c1218ac628
Evan Friday <evanjohnfriday@gmail.com>
2016-01-25 17:05:02 -0800

    hiding edit date for non admin

--- open_playsheet.php
+++ open_playsheet.php
@@ -30,14 +30,14 @@
 			<div class='scroll<?php if(strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'firefox') > 0 ){echo " firefox'"; }?>' scrolly='!open_playsheet.loading ? open_playsheet.more():""'>
 
 				<table class='open_playsheet table-hover table-condensed'>
-					<tr><th>Show<th>Host<th>Date<th>Status<th>Edit Date<th ng-if='open_playsheet.is_admin'></tr>
+					<tr><th>Show<th>Host<th>Date<th>Status<th ng-if='open_playsheet.is_admin'>Edit Date<th ng-if='open_playsheet.is_admin'></tr>
 					<tbody when-scrolled='more()' >
 						<tr ng-model='open_playsheet.playsheets' ng-repeat='item in open_playsheet.playsheets track by $index'>
 								<td onclick='go(this)' data-href='playsheet_angular.php?id={{item.id}}&socan={{item.socan}}' >{{item.show_name}}</td>
 								<td onclick='go(this)' data-href='playsheet_angular.php?id={{item.id}}&socan={{item.socan}}' class='host'>{{item.host}}</td>
 								<td onclick='go(this)' data-href='playsheet_angular.php?id={{item.id}}&socan={{item.socan}}' >{{item.start_time}}</td>
 								<td onclick='go(this)' data-href='playsheet_angular.php?id={{item.id}}&socan={{item.socan}}' >{{item.status != 2 ? "(Draft)" : "" }}</td>
-								<td onclick='go(this)' data-href='playsheet_angular.php?id={{item.id}}&socan={{item.socan}}' class='edit_date'>{{item.edit_date}}</td>
+								<td ng-if='open_playsheet.is_admin' onclick='go(this)' data-href='playsheet_angular.php?id={{item.id}}&socan={{item.socan}}' class='edit_date'>{{item.edit_date}}</td>
 								<td ng-if='open_playsheet.is_admin'><button ng-click='open_playsheet.delete(item.id)' type='button' class='delete'>Delete</button></td>
 						</tr>
 					<tbody>
