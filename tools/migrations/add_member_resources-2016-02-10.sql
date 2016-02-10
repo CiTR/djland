15026057d4f7fd6a790dadac1eb2d700de1b3410
Evan Friday <evanjohnfriday@gmail.com>
2016-02-10 11:19:29 -0800

    adding member resource table create statement

---
+++ tools/migrations/add_member_resources-2016-02-10.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `member_resources` (
+  `index` int(10) NOT NULL AUTO_INCREMENT,
+  `blurb` tinytext,
+  `link` tinytext,
+  `link_name` tinytext,
+  `type` varchar(45) DEFAULT 'general',
+  `CREATED_AT` datetime DEFAULT NULL,
+  `UPDATED_AT` datetime DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
