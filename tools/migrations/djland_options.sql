d531e9b19e9b54a4f2b750a7ab181a1ff9cde00a
evanfriday <evanjohnfriday@gmail.com>
2016-04-04 12:06:32 +1000

    updating create statement

--- tools/migrations/djland_options.sql
+++ tools/migrations/djland_options.sql
@@ -2,5 +2,8 @@ CREATE TABLE IF NOT EXISTS `djland_options` (
   `index` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `djland_option` tinytext COLLATE utf8_bin NOT NULL,
   `value` text COLLATE utf8_bin NOT NULL,
+  `CREATED_AT` datetime NOT NULL,
+  `UPDATED_AT` datetime NOT NULL,
   PRIMARY KEY (`index`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+INSERT INTO `djland_options` (index,djland_option,value) VALUES (1,'membership_cutoff','2014/2015');
