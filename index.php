fd80bc1ae99e82179d59b7bb783f3c22bc111b1c
Scott Pidzarko <scott.pidzarko@gmail.com>
2016-05-11 17:00:55 -0700

    Making the small white box from the login page go away and the error message in a better spot when you have the incorrect password

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once("headers/password.php");
 //header("HTTP/1.0 302 Redirect\r\n");
 if( isset($_POST['action']) && $_POST['action'] == "signup"){
 	header("Location: membership_add.php");
-	//echo "signing up"; 
+	//echo "signing up";
 	//printf("<html><head><meta http-equiv=\"refresh\" content=\"0;URL=membership_add.php\"><link rel=stylesheet href=css/style.css type=text/css></head></html>");
 	}
 else if(is_logged_in() && isset($_GET['action']) && $_GET['action'] == "logout") {
@@ -17,7 +17,7 @@ else if(is_logged_in() && isset($_GET['action']) && $_GET['action'] == "logout")
 else if(is_logged_in()) {
 	//header("Location: main.php");
 	printf("<html><head><meta http-equiv=\"refresh\" content=\"0;URL=main.php\"><link rel=stylesheet href=css/style.css type=text/css></head></html>");
-	}	
+	}
 else if(isset($_POST['action']) && $_POST['action'] == "login") {
 	//isset($_POST['login']) && isset($_POST['password'])
 	if(login ($_POST['username'], $_POST['password'], isset( $_POST['permanent_cookie'] ) ) ) {
@@ -26,7 +26,7 @@ else if(isset($_POST['action']) && $_POST['action'] == "login") {
 
 		}
 	else{
-		$message = "Login (Failed) ".login($_POST['username'], $_POST['password'], isset($_POST['permanent_cookie']) );
+		$message = "Login Failed <br> (Incorrect Password)".login($_POST['username'], $_POST['password'], isset($_POST['permanent_cookie']) );
 		}
 	}
 else {
@@ -52,18 +52,20 @@ else {
 	else{
 		echo "<body class='wallpaper'>";
 	}
-?>	
+?>
 	<div id = 'login'>
 		<FORM METHOD=POST ACTION= <?php echo "'".$_SERVER['SCRIPT_NAME']."'"; ?> name='site_login' >
 			<h3>Welcome to DJ Land</h3>
-			<label for='username'>Login: </label><input type=text name='username'/><br/>
-			<label for='password'>Password: </label><input type=password name='password'/><br/>
+			<label for='username'>Login: </label><input type=text name='username' class='right double-margin-right'/><br/>
+			<label for='password'>Password: </label><input type=password name='password' class='right double-margin-right'/><br/>
 			<input type='submit' name='action' value='login'/>
 			<input type='submit' name='action' value='signup'/>
 		</FORM>
 		If you forget your password, please email Hugo at volunteer@citr.ca
 		<div id = 'message' >
-		<?php echo $message; ?>
+		<?php
+			echo $message;
+		?>
 		</div>
 	</div>
 
