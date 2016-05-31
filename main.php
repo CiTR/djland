0799dd4a3f665ca53147ac54f627492ee3ae2c17
Scott Pidzarko <scott.pidzarko@gmail.com>
2016-05-31 14:08:28 -0700

    Added a license and attribution footer for the landing and login pages

--- main.php
+++ main.php
@@ -1,13 +1,14 @@
 <?php
 include_once("headers/session_header.php");
 require_once("headers/menu_header.php");
+include_once("license_footer.php");
 ?>
 <html>
 	<head>
 		<meta name=ROBOTS content=\"NOINDEX, NOFOLLOW\">
 		<link rel=stylesheet href=css/style.css type=text/css>
 		<script src='js/jquery-1.11.3.min.js'></script>
-		
+
 		<title>DJ Land</title>
 	</head>
 	<body class='wallpaper'>
