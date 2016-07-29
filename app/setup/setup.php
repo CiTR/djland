225ad3a76ee0a41e1317c33663d5c7d3b6e2cce6
Evan Friday <evanjohnfriday@gmail.com>
2016-07-29 16:17:19 -0700

    starting auto-setup file

---
+++ app/setup/setup.php
@@ -0,0 +1,23 @@
+<?php if(file_exists($_SERVER['DOCUMENT_ROOT']."../config.php")) return; ?>
+<html>
+	<head>
+		<link rel='stylesheet' href='css/bootstrap.min.css'></script>
+	</head>
+	<body>
+		<h1>DJLand Setup</h1>
+		
+
+	</body>
+<?php
+
+
+
+
+
+
+
+
+
+
+ ?>
+ </html>
