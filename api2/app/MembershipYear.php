0c1bb751303593e4bb182ca67b8ad3e0cfeb8087
CiTR Tech <technicalservices@citr.ca>
2016-11-17 15:25:51 -0800

    Update MembershipYear.php
    Reflecting DB changes

--- api2/app/MembershipYear.php
+++ api2/app/MembershipYear.php
@@ -10,7 +10,7 @@ class MembershipYear extends Model
     const CREATED_AT = 'create_date';
     const UPDATED_AT = 'edit_date';
     protected $primaryKey = 'member_id';
-    protected $fillable = array('member_id', 'membership_year', 'paid', 'sports', 'news', 'arts', 'music', 'show_hosting', 'live_broadcast', 'tech', 'programming_committee', 'ads_psa', 'promotions_outreach', 'discorder', 'discorder_2', 'digital_library', 'photography', 'tabling', 'dj', 'other', 'womens_collective', 'indigenous_collective', 'accessibility_collective', 'create_date', 'edit_date');
+    protected $fillable = array('member_id', 'membership_year', 'paid', 'sports', 'news', 'arts', 'music', 'show_hosting', 'live_broadcast', 'tech', 'programming_committee', 'ads_psa', 'promotions_outreach', 'discorder_illustrate', 'discorder_write', 'digital_library', 'photography', 'tabling', 'dj', 'other', 'womens_collective', 'indigenous_collective', 'accessibility_collective', 'create_date', 'edit_date');
 
     function member(){
     	return $this->belongsTo('App\Member');
