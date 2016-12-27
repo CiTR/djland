2b407423ba3ec123d6130adf1c6e9c96aa1ba0b5
Scott Pidzarko <scott.pidzarko@gmail.com>
2016-12-26 16:54:15 -0800

    DJLandconnector routes updated to also support ?= query string parameter passing since that's what wp already uses

--- app/api2/app/Http/Routes/djlandconnector.php
+++ app/api2/app/Http/Routes/djlandconnector.php
@@ -11,54 +11,49 @@ use App\Podcast as Podcast;
 
 Route::group(array('prefix'=>'DJLandConnector'),function(){
 	Route::group(array('prefix'=>'show'),function(){
+		//?= query string style for just one show with given ID
+		Route::get('/',function(){
+			$id= Input::get('ID');
+			return show($id);
+		});
 		//just one show with given ID
 		Route::get('/{id}',function($id=id){
-			//First we get the needed info from the shows table
-			if(!Show::find($id)) return null;
-			$data = Show::select('id as show_id',
-			      'name',
-			      'last_show',
-			      'create_date',
-				  'edit_date',
-				  //above Was previously (not sure if this matters?):
-			      //GREATEST(shows.edit_date,'0000-00-00 00:00:00') as edit_date,
-			      'active',
-			      'primary_genre_tags',
-			      'secondary_genre_tags',
-			      'website',
-			      'rss',
-			      'show_desc',
-			      'alerts',
-			      'image as show_img',
-			      'host as host_name',
-			      'podcast_title as podcast_title',
-			      'podcast_subtitle AS podcast_subtitle',
-			      'secondary_genre_tags as podcast_keywords',
-			      'image as podcast_image_url',
-			      'podcast_xml')->where('id','=',$id)->get();
-			//And all the social links for that show from the social table
-			$data['social_links'] = Social::select('social_name','social_url')->where('show_id','=',$id)->get();
-			return Response::json( $data );
+			return show($id);
 		});
 	});
 	Route::group(array('prefix'=>'shows'),function(){
-		//Return list of shows in LIMIT/OFFSET FORMAT
+		//Return list of shows in LIMIT/OFFSET format
 		Route::get('/{LIMIT}/{OFFSET}',function($limit=LIMIT,$offset=OFFSET){
 			return Show::select('id','edit_date')->offset($offset)->limit($limit)->get();
 		});
+		//?= query string style for LIMIT/OFFSET format
+		Route::get('/', function(){
+			//Return list of shows in LIMIT/OFFSET FORMAT
+			$limit=Input::get('LIMIT');
+			$offset=Input::get('OFFSET');
+			return Show::select('id','edit_date')->offset($offset)->limit($limit)->get();
+		});
 	});
+	//Returns a playlist given playlist ID
 	Route::group(array('prefix'=>'playlist'),function(){
+		// /ID/ style
 		Route::get('/{id}',function($id=id){
-			$playsheet = Playsheet::select('id as playlist_id', 'show_id', 'start_time', 'end_time', 'edit_date', 'type as playlist_type', 'host as host_name')->where('id','=',$id)->get();
-			$podcast = Podcast::select('id as episode_id', 'summary as episode_description', 'title as episode_title', 'url as episode_audio')->where('playsheet_id','=',$id)->get();
-			//For some reason ->merge() didn't work so we did this and it did
-			$ret = array_merge($playsheet[0]->toArray(), $podcast[0]->toArray());
-			$ret['songs'] = Playsheet::find($id)->playitems;
-			return Response::json($ret);
+			return playlist($id);
+		});
+		//?= query string style
+		Route::get('/',function(){
+			$id=Input::get('ID');
+			return playlist($id);
 		});
 	});
 	Route::group(array('prefix'=>'playlists'),function(){
 		Route::get('/{limit}/{offset}',function($limit=limit, $offset=offset){
+			return Show::select('id','edit_date')->playsheets()->offset($offset)->limit($limit)->get();
+		});
+		//?= query string style
+		Route::get('/{limit}/{offset}',function(){
+			$limit=Input::get('LIMIT');
+			$offset=Input::get('OFFSET');
 			return Show::select('id','edit_date')->playsheets()->offset($offset)->limit($limit)->get();
 		});
 	});
@@ -77,3 +72,46 @@ Route::group(array('prefix'=>'DJLandConnector'),function(){
 	});
 	*/
 });
+
+//************
+//Functions
+//************
+
+//Just one show with a given show ID
+function show($id){
+	//First we get the needed info from the shows table
+	if(!Show::find($id)) return null;
+	$data = Show::select('id as show_id',
+		  'name',
+		  'last_show',
+		  'create_date',
+		  'edit_date',
+		  //above Was previously (not sure if this matters?):
+		  //GREATEST(shows.edit_date,'0000-00-00 00:00:00') as edit_date,
+		  'active',
+		  'primary_genre_tags',
+		  'secondary_genre_tags',
+		  'website',
+		  'rss',
+		  'show_desc',
+		  'alerts',
+		  'image as show_img',
+		  'host as host_name',
+		  'podcast_title as podcast_title',
+		  'podcast_subtitle AS podcast_subtitle',
+		  'secondary_genre_tags as podcast_keywords',
+		  'image as podcast_image_url',
+		  'podcast_xml')->where('id','=',$id)->get();
+	//And all the social links for that show from the social table
+	$data['social_links'] = Social::select('social_name','social_url')->where('show_id','=',$id)->get();
+	return Response::json( $data );
+}
+//Get playlist given  playlist ID
+function playlist($id){
+	$playsheet = Playsheet::select('id as playlist_id', 'show_id', 'start_time', 'end_time', 'edit_date', 'type as playlist_type', 'host as host_name')->where('id','=',$id)->get();
+	$podcast = Podcast::select('id as episode_id', 'summary as episode_description', 'title as episode_title', 'url as episode_audio')->where('playsheet_id','=',$id)->get();
+	//For some reason ->merge() didn't work so we did this and it did
+	$ret = array_merge($playsheet[0]->toArray(), $podcast[0]->toArray());
+	$ret['songs'] = Playsheet::find($id)->playitems;
+	return Response::json($ret);
+}
