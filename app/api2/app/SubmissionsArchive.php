48de827fbab5cdc658e21c4cc0756c79fdd80994
Scott Pidzarko <scott.pidzarko@gmail.com>
2017-03-05 15:53:57 -0800

    Adding created_at and updated_at to SubmissionsArchive.php

--- app/api2/app/SubmissionsArchive.php
+++ app/api2/app/SubmissionsArchive.php
@@ -6,5 +6,5 @@ use InvalidArgumentException;
 class SubmissionsArchive extends Model
 {
     protected $table = 'submissions_archive';
-    protected $fillable = array( 'id','contact','catalog','artist','title','submitted','format_id','cancon','femcon','local','label','review_comments' );
+    protected $fillable = array( 'id','contact','catalog','artist','title','submitted','format_id','cancon','femcon','local','label','review_comments', 'created_at', 'updated_at' );
 }
