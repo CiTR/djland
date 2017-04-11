ce124fccce1b9878ea7c0e0dc9eac93274095150
Scott Pidzarko <scott.pidzarko@gmail.com>
2017-04-10 23:21:34 -0700

    LibrarySongs Model + get Route done

--- app/api2/app/Library.php
+++ app/api2/app/Library.php
@@ -7,4 +7,8 @@ class Library extends Model
 {
     protected $table = 'library';
     protected $fillable = array( 'id','format_id','catalog','crtc','cancon','femcon','local','playlist','compilation','digitized','status','artist','title','label','genre','added','modified','songlist_id','description','email','updated_at','created_at' );
+
+    public function songs(){
+        return $this->hasMany('App\LibrarySongs','library_id');
+    }
 }
