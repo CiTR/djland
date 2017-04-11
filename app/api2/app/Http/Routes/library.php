5a5f743d9ab577659362174723f90041048d7288
Scott Pidzarko <scott.pidzarko@gmail.com>
2017-04-11 06:38:29 -0700

    Route to move a submission into library done

--- app/api2/app/Http/Routes/library.php
+++ app/api2/app/Http/Routes/library.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Library_Edit as Edits;
 use App\Library as Library;
-use App\Member as Member;
+use App\LibrarySongs as LibrarySongs;
+use App\Submision as Submission;
+use App\SubmissionsSongs as SubmissionsSongs;
 use Carbon\Carbon;
 use Validator as Validator;
 
@@ -91,6 +92,108 @@ use Validator as Validator;
                 'compilation' => Input::get('compilation'),
                 'digitized' => Input::get('in_sam')
             ]);
+        });
+        Route::post('/fromsubmissions',function(){
+            $submission_id = Input::get('submission_id');
+            if(!is_numeric(Input::get('format')))
+                switch(Input::get('format')){
+                    //TODO: look this up in DB
+                    case "CD":
+                        $format = 1;
+                        break;
+                    case "cd":
+                        $format = 1;
+                        break;
+                    case "LP":
+                        $format = 2;
+                        break;
+                    case "7i":
+                        $format = 3;
+                        break;
+                    case "7\"":
+                        $format = 3;
+                        break;
+                    case "CASS":
+                        $format = 4;
+                        break;
+                    case "CART":
+                        $format = 5;
+                        break;
+                    case "MP3":
+                        $format = 6;
+                        break;
+                    case "mp3":
+                        $format = 6;
+                        break;
+                    case "mP3":
+                        $format = 6;
+                        break;
+                    case "Mp3":
+                        $format = 6;
+                        break;
+                    case "MD":
+                        $format = 7;
+                        break;
+                    case "??":
+                        $format = 8;
+                        break;
+                    default:
+                        $format = 8;
+            } else {
+                $format = Input::get('format');
+            }
+
+            if(Input::get('playlist') == 1){
+                $status = 'P';
+            } else {
+                $status = 'A';
+            }
+
+            $lib =  Library::create([
+                'catalog' => Input::get('catalog'),
+                'format_id' => $format,
+                'status' => $status,
+                'artist' => Input::get('artist'),
+                'title' => Input::get('album_title'),
+                'label' => Input::get('label'),
+                'genre' => Input::get('genre'),
+                'cancon' => Input::get('cancon'),
+                'femcon' => Input::get('femcon'),
+                'local' => Input::get('local'),
+                'compilation' => Input::get('compilation'),
+                'digitized' => Input::get('in_sam'),
+                'crtc' => Input::get('crtc')
+            ]);
+            if($lib['id'] > 0){
+                $return_array;
+                $i = 0;
+                foreach(SubmissionsSongs::where('submission_id', '=', $submission_id)->get() as $submission_song){
+                    $return_array[$i] = LibrarySongs::create([
+                        'library_id' => $lib['id'],
+                        'artist' => $submission_song['artist'],
+                        'album_artist' => $submission_song['album_artist'],
+                        'song_title' => $submission_song['song_title'],
+                        'album_title' => $submission_song['album_title'],
+                        'credit' => $submission_song['credit'],
+                        'track_num' => $submission_song['track_num'],
+                        'tracks_total' => $submission_song['tracks_total'],
+                        'genre' => $submission_song['genre'],
+                        's/t' => $submission_song['s/t'],
+                        'v/a' => $submission_song['v/a'],
+                        'compilation' => Input::get('compilation'),
+                        'composer' => $submission_song['composer'],
+                        'crtc' => Input::get('crtc'),
+                        'year' => $submission_song['year'],
+                        'length' => $submission_song['length'],
+                        //TODO: file_location
+                        'file_location' => ""
+                    ]);
+                    $i++;
+                }
+                return $return_array;
+            }else{
+                return response("Unable to create library record. Please try again", 500);
+            }
         });
     });
 //});
