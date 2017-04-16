30b225de3ef4fb8d077069dbbd03380380e799e8
Scott Pidzarko <scott.pidzarko@gmail.com>
2017-04-15 19:53:27 -0700

    Submissions_Rejected to SubmissionsRejected

---
+++ app/api2/app/SubmissionsRejected.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
+
+class SubmissionsRejected extends Model
+{
+    protected $table = 'submissions_rejected';
+    protected $fillable = array( 'id','email','artist','title','submitted' );
+}
