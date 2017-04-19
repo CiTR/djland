f69b44c8a5b402bd2bd5f386c4a78c07c4c911f5
Scott Pidzarko <scott.pidzarko@gmail.com>
2017-04-18 17:25:57 -0700

    Move to submissions rejected table when the cleanup job for rejected submissions runs

--- app/api2/app/Console/Kernel.php
+++ app/api2/app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Submissions as Submissions;
 use Carbon\Carbon;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -29,10 +30,33 @@ class Kernel extends ConsoleKernel
         //Example task
         //$schedule->command('inspire')
         //         ->hourly();
+
         //Task to delete submissions in the trash over a month old
         //Using Cabon magic from http://carbon.nesbot.com/docs/#Difference
         $schedule->call(function () {
-                     Submissions::where('is_trashed','=',1)->where('updated_at','<',Carbon::now()->subMonth())->delete();
+                    //Move all entries to the Rejected table
+                    $submissions = Submissions::where('is_trashed','=',1)->where('updated_at','<',Carbon::now()->get();
+                    foreach($submission as $submissons){
+                        $rejected = Rejected::create([
+                            'artist' => $submission['artist'],
+                            'title' => $submission['title'],
+                            'contact' => $submission['contact'],
+                            'label' => $submission['label'],
+                            'cancon' => $submission('cancon'),
+                            'femcon' => $submission('femcon'),
+                            'local' => $submission('local'),
+                            'description' => $submission['description'],
+                            'catalog' => $submission['catalog'],
+                            'format_id' => $submission('format_id'),
+                            'submitted' => $submission('submitted'),
+                            'review_comments' => $submission['review_comments'],
+                        ]);
+                        //Log that we deleted things
+                        Log::notice("Moved rejected submisson from the submission table to the rejected table. Created:");
+                        Log::notice($rejected);
+                    }
+                    //And then delete
+                    Submissions::where('is_trashed','=',1)->where('updated_at','<',Carbon::now()->subMonth())->delete();
                  })->dailyAt('04:00');
     }
 }
