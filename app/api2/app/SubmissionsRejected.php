a1eb7f76b910a590a99c912e60ee5ea22c62ff56
Scott Pidzarko <scott.pidzarko@gmail.com>
2017-04-22 17:50:56 -0700

    Renaming class to not use underscore (remember to php artisan optimize)

--- app/api2/app/SubmissionsRejected.php
+++ app/api2/app/SubmissionsRejected.php
@@ -4,7 +4,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use InvalidArgumentException;
 
-class Submissions_Rejected extends Model
+class SubmissionsRejected extends Model
 {
     protected $table = 'submissions_rejected';
     protected $fillable = array( 'id','email','artist','title','submitted' );
