83daafb9dd23f2e5576b57d486a5d5339fab2f9e
Saw-mon & Natalie <samangharib50233@gmail.com>
2018-01-22 18:25:24 -0800

    changed the regular expression in getSAMRange

--- app/js/api.js
+++ app/js/api.js
@@ -68,7 +68,7 @@ angular.module('djland.api', []).factory('call', function ($http, $location) {
 			return $http.get(API_URL_BASE+"/SAM/recent/"+offset || 0);
 		},
 		getSamRange: function(from,to){
-            var re = /^\d{4}-\d{2}-\d{2}%20\d{2}:\d{2}:\d{2}$/g;
+            var re = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/g;
             if(re.test(from) && re.test(to)){
                 return $http.get(API_URL_BASE+"/SAM/range?from="+from+"&to="+to);
             }
