5d264f201cb25fb11892f9130ee9395f473cea12
Saw-mon-and-Natalie <samangharib50233@gmail.com>
2018-05-28 16:37:23 -0700

    adding training videos

---
+++ app/training_videos.php
@@ -0,0 +1,90 @@
+<?php
+
+include_once("headers/session_header.php");
+require_once("headers/security_header.php");
+#require_once("headers/functions.php");
+require_once("headers/menu_header.php");
+
+error_reporting(E_ALL);
+?>
+
+<html>
+	<head>
+		<meta name=ROBOTS content="NOINDEX, NOFOLLOW">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+	</head>
+
+	<body class='wallpaper'>
+
+	<?php print_menu(); ?>
+
+		<br/>
+		<h1>Training Videos</h1>
+
+		<div class="center">
+			<div class="center" style="max-width:830px">
+                <p>Has it been a long time since you've completed your training and need a refresher?
+                    Or just starting out and want to get some experience before you head into your training session?
+                    Or even out of the Vancouver area and can't make it down to the studio yet?
+                    Then this is the training series is for you to gain a brief summary of some of that knowledge.
+                </p>
+
+                <p>Note that these videos are not substitutions for a full training session.
+                    If you wish to be certifiably trained, please sign up for training at the CiTR
+                    or email volunteer@citr.ca to book ahead.
+                </p>
+                <h2>Contents</h2>
+                <ul>
+                    <li><a href="#Installment1">Installment 1: Using DJLAND</a></li>
+                    <li><a href="#Installment2">Installment 2: Recording Audio in Audacity</a></li>
+                    <li><a href="#Installment3">Installment 3: Audacity Tools Basics</a></li>
+                    <li><a href="#Installment4">Installment 4: Saving in Audacity</a></li>
+                    <li><a href="#Installment5">Installment 5: Using the Mics in the Studio</a></li>
+                    <li><a href="#Installmenl6">Installment 6: Boards and Previewing</a></li>
+                    <li><a href="#Installment7">Installment 7: Recording an On-Air Call</a></li>
+                    <li><a href="#Installment8">Installment 8: Cues in SAM</a></li>
+                </ul>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment1"><h1>Installment 1: Using DJLAND</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment2"><h1>Installment 2: Recording Audio in Audacity</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment3"><h1>Installment 3: Audacity Tools Basics</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment4"><h1>Installment 4: Saving in Audacity</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment5"><h1>Installment 5: Using the Mics in the Studio</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment6"><h1>Installment 6: Boards and Previewing</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment7"><h1>Installment 7: Recording an On-Air Call</h1></a>
+            </div>
+            <br />
+            <br />
+            <div class="center" style="max-width:830px">
+                <a name="#Installment8"><h1>Installment 8: Cues in SAM</h1></a>
+            </div>
+		</div>
+	</body>
+</html>
