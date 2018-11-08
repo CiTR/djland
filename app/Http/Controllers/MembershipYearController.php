5057cac85215f4ab95f0cd5aa72fd72505db417e
Tanner McColeman <mccolemantanner@gmail.com>
2018-11-08 12:32:04 -0800

    Add MembershipYear model/migration and empty controller

---
+++ app/Http/Controllers/MembershipYearController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MembershipYear;
+use Illuminate\Http\Request;
+
+class MembershipYearController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MembershipYear  $membershipYear
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MembershipYear $membershipYear)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MembershipYear  $membershipYear
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MembershipYear $membershipYear)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MembershipYear  $membershipYear
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MembershipYear $membershipYear)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MembershipYear  $membershipYear
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MembershipYear $membershipYear)
+    {
+        //
+    }
+}
