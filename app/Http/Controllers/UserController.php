45afc47905aaa0e655415e610931bff747221314
raghavaggsss <zacefron.efron727@gmail.com>
2018-12-17 13:59:20 -0800

    Removed MemberShipYear. Renamed MemberController to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MemberController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('members', 'MemberController');
+Route::resource('members', 'UserController');
