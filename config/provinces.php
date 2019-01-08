1def711bdedc04d488f4027318a89add46c6f7ad
Tanner McColeman <mccolemantanner@gmail.com>
2019-01-07 17:11:21 -0800

    Formatting on provinces

--- config/provinces.php
+++ config/provinces.php
@@ -1,7 +1,17 @@
 <?php
 
-return ["AB" => "Alberta", "BC" => "British Columbia", "MB" => "Manitoba",
-    "NB" => "New Brunswick", "NL" => "Newfoundland and Labrador", "NT" => "Northwest Territories",
-    "NS" => "Nova Scotia", "NU" => "Nunavut", "ON" => "Ontario",
-    "PE" => "Prince Edward Island", "QC" => "Quebec", "SK" => "Saskatchewan",
-    "YT" => "Yukon",];
+return [
+    "AB" => "Alberta",
+    "BC" => "British Columbia",
+    "MB" => "Manitoba",
+    "NB" => "New Brunswick",
+    "NL" => "Newfoundland and Labrador",
+    "NT" => "Northwest Territories",
+    "NS" => "Nova Scotia",
+    "NU" => "Nunavut",
+    "ON" => "Ontario",
+    "PE" => "Prince Edward Island",
+    "QC" => "Quebec",
+    "SK" => "Saskatchewan",
+    "YT" => "Yukon",
+];
