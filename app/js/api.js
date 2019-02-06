d89bea76c19e6ce5fdfcfa36fa806772040e6b07
Tanner McColeman <mccolemantanner@gmail.com>
2019-02-05 17:31:49 -0800

    Fix javascript error on playsheet

--- app/js/api.js
+++ app/js/api.js
@@ -68,17 +68,17 @@ angular.module('djland.api', []).factory('call', function ($http, $location) {
 			return $http.get(API_URL_BASE+"/SAM/recent/"+offset || 0);
 		},
 		getSamRange: function(from,to){
-            var re = /^\d{4}-\d{2}-\d{2}.*/;
-            if(re.test(from) && re.test(to)){
+            // var re = /^\d{4}-\d{2}-\d{2}.*/;
+            // if(re.test(from) && re.test(to)){
                 return $http.get(API_URL_BASE+"/SAM/range?from="+from+"&to="+to);
-            }
-            else{
-                console.log("Invalid ranges for getSamRange!");
-                console.log(from);
-                console.log(to);
-                console.log("-------");
-                return [];
-            }
+            // }
+            // else{
+            //     console.log("Invalid ranges for getSamRange!");
+            //     console.log(from);
+            //     console.log(to);
+            //     console.log("-------");
+            //     return [];
+            // }
 		},
 		getAdSchedule: function(){
 			return $http.get(API_URL_BASE+'/adschedule');
